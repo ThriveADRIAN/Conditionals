3d699ed5c19bacc40fc81789bf1267dc5a1ece75
Adrian Cunanan <adrian.cunanan@me.com>
2013-04-05 11:23:29 -0400

    Chalenge Completed

--- Conditionals/main.c
+++ Conditionals/main.c
@@ -11,8 +11,18 @@
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    printf("Hello, World!\n");
+    int i = 20;
+    int j = 25;
+    
+    int k = ( i > j ) ? 10 : 5;
+    
+    if ( 5 < j - k ) { //first expression
+        printf("The first expression is true.");
+    } else if ( j > i ) { //second expression
+        printf("The second expression is true.");
+    } else {
+        printf("Neither expression is true.");
+    }
     return 0;
 }
 
